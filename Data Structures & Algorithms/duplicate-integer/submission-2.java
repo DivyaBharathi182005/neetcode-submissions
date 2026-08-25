ce5d242dbeaa9cded3d20d353e4bdea2e332fcc0
DivyaBharathi182005 <divyabharathimohana2003@gmail.com>
2026-08-25 18:24:47 +0530

    Add: duplicate-integer - submission-2

---
+++ Data Structures & Algorithms/duplicate-integer/submission-2.java
@@ -0,0 +1,20 @@
+class Solution {
+    public boolean hasDuplicate(int[] nums) {
+
+        HashSet<Integer> set = new HashSet<>();
+
+        for (int x : nums) {
+
+            if (set.contains(x)) {
+                return true;
+            }
+
+            set.add(x);
+        }
+
+        return false;
+    }
+}
+
+
+
